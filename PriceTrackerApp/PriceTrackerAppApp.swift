e5cc2478287262b3bbfb51cefa3606d04015ba3a
Fabio Turchet <turchet.fabio@gmail.com>
2026-02-26 19:37:00 +0100

    Initial Commit

---
+++ PriceTrackerApp/PriceTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PriceTrackerAppApp.swift
+//  PriceTrackerApp
+//
+//  Created by Fabiolous on 26/2/26.
+//
+
+import SwiftUI
+
+@main
+struct PriceTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
