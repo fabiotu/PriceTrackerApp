ea0a13a3a246f56aed5c3557418ef3c58b5df120
Fabio Turchet <turchet.fabio@gmail.com>
2026-02-28 11:36:00 +0100

    Added unit tests

--- PriceTrackerAppTests/PriceTrackerAppTests.swift
+++ PriceTrackerAppTests/PriceTrackerAppTests.swift
@@ -6,30 +6,37 @@
 //
 
 import XCTest
+@testable import PriceTrackerApp
 
-final class PriceTrackerAppTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+@MainActor
+final class AssetTests: XCTestCase {
+    
+    func testAssetImmutabilityAndTrend() {
+        let initialAsset = Asset(symbol: "AAPL", price: 100.0)
+        XCTAssertEqual(initialAsset.trend, .flat)
+        
+        let upAsset = initialAsset.updating(with: 105.0)
+        XCTAssertEqual(upAsset.price, 105.0)
+        XCTAssertEqual(upAsset.trend, .up)
+        XCTAssertNotEqual(initialAsset.lastUpdated, upAsset.lastUpdated)
+        
+        let downAsset = upAsset.updating(with: 95.0)
+        XCTAssertEqual(downAsset.trend, .down)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testStoreReceivesUpdatesFromMockService() async throws {
+        let mockService = MockWebSocketService()
+        let store = AssetStore(webSocketService: mockService)
+        
+        XCTAssertFalse(store.isFeedActive)
+        
+        store.toggleFeed()
+        XCTAssertTrue(store.isFeedActive)
+        
+        try await Task.sleep(for: .seconds(1.5))
+        
+        XCTAssertEqual(store.connectionState, .connected)
+        
+        store.toggleFeed()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
